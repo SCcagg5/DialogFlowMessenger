5e87489d73b4f1b11168064e6e245fbd6fe43a2a
SCcagg5 <sccagg5@gmail.com>
2019-04-30 10:16:54 +0200

    Update server.py

--- back-end/src/server.py
+++ back-end/src/server.py
@@ -45,9 +45,12 @@ def base():
 
     if not toret.err:
         sent = sentiment(params["token"], params["lang"])
-        if not check.contain(params, ["analysis"]) or not params["analysis"]:
-            err = sent.talk(params["sentence"])
-        else:
+        try:
+            if not params["analysis"]:
+                err = sent.talk(params["sentence"])
+            else:
+                err = sent.getnewtoken()
+        excxept:
             err = sent.getnewtoken()
         if not err[0]:
             toret.add_error(err[1], err[2])
